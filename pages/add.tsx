0d93e8d4379e5bc7a3fe61db89aa00939e9b5ed3
Vipul Dessai <badgamerbad@gmail.com>
2023-12-23 00:28:10 +0530

    inti

---
+++ pages/add.tsx
@@ -0,0 +1,43 @@
+import Head from "next/head";
+import Link from "next/link";
+
+import commonStyles from "../styles/common.module.scss";
+
+export default function Home() {
+  return (
+    <div className={commonStyles.container}>
+      <Head>
+        <title>Add Note</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>
+        <img src="/vercel.svg" alt="Vercel" className={commonStyles.logo} />
+      </header>
+
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Back</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <main>
+        <p>Add note page</p>
+      </main>
+
+      <footer>
+        <a
+          className={commonStyles.flexCenter}
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span>Powered by</span>
+          <img src="/vercel.svg" alt="Vercel" className={commonStyles.logo} />
+        </a>
+      </footer>
+    </div>
+  );
+}
