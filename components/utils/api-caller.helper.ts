c4a4309c7df965fb79024c7715c764bdd9efda61
Vipul Dessai <badgamerbad@gmail.com>
2024-01-23 02:51:29 +0530

    cors check - dummy graphql call

--- components/utils/api-caller.helper.ts
+++ components/utils/api-caller.helper.ts
@@ -9,12 +9,34 @@ export const postData = async (url: string, payload: any) => {
   let config = {
     method: "post",
     maxBodyLength: Infinity,
-    url: "https://7ryqgloudd.execute-api.us-east-1.amazonaws.com/api/values",
+    // url: "https://7ryqgloudd.execute-api.us-east-1.amazonaws.com/api/values",
+    url: "https://7ryqgloudd.execute-api.us-east-1.amazonaws.com/graphql",
+    // url: "https://localhost:62926/api/values",
     headers: {
       "Content-Type": "application/json",
     },
-    data: JSON.stringify({}),
+    data: JSON.stringify({
+      query: `query {
+        books {
+          id
+          author {
+            name
+          }
+        }
+      }`,
+      variables: {},
+    }),
   };
   const r = await axios.request(config);
   return r.data;
 };
+
+axios.interceptors.request.use(
+  (config) => {
+    const { headers } = config;
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  },
+);
