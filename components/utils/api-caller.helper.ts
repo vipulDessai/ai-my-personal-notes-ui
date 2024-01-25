30237784d9d6f88d8dcc9acad4824d2b26ce3dfa
Vipul Dessai <badgamerbad@gmail.com>
2024-01-25 19:16:28 +0530

    graph ql - testing axios post request with authorization

--- components/utils/api-caller.helper.ts
+++ components/utils/api-caller.helper.ts
@@ -10,19 +10,17 @@ export const postData = async (url: string, payload: any) => {
     method: "post",
     maxBodyLength: Infinity,
     // url: "https://7ryqgloudd.execute-api.us-east-1.amazonaws.com/api/values",
-    url: "https://7ryqgloudd.execute-api.us-east-1.amazonaws.com/graphql",
-    // url: "https://localhost:62926/api/values",
+    // url: "https://7ryqgloudd.execute-api.us-east-1.amazonaws.com/graphql",
+    // url: "https://localhost:8081/api/values",
+    url: "https://localhost:8081/graphql",
     headers: {
       "Content-Type": "application/json",
+      Authorization:
+        "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIwYmFmMGU0Ni1jMTRjLTRhN2ItYTk2ZC02NjYyNGJkM2E0OGIiLCJuYW1lIjoiaHJsZWFkZXJAZXhhbXBsZS5jb20iLCJyb2xlIjpbImhyIiwibGVhZGVyIl0sIm5iZiI6MTcwNjE5MDIwNiwiZXhwIjoxNzEzOTY2MjA2LCJpYXQiOjE3MDYxOTAyMDYsImlzcyI6Imlzc3VlciIsImF1ZCI6ImF1ZGllbmNlIn0.i0MRrHBrAaXLYjafp6X6DkHQ_Fjxq-0TlJk4Q681K60",
     },
     data: JSON.stringify({
-      query: `query {
-        books {
-          id
-          author {
-            name
-          }
-        }
+      query: `query authorize {
+        authorized
       }`,
       variables: {},
     }),
